11b1ec0eaf91ed554eb001ee5146b9c97eac18d0
MirnaPamelaDiaz <pamela_diaz_24@outlook.com>
2023-02-28 14:28:40 -0300

    Add se agregan las carpetas del proyecto

---
+++ src/test/java/com/proyecto/b/s/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.b.s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
