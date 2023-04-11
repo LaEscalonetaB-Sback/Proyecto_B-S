a4bf7b2e54217fd603c454dda48da5c7141bde64
MirnaPamelaDiaz <pamela_diaz_24@outlook.com>
2023-04-11 17:01:17 -0300

    Feature se crea ennpoint de entidad Answer

---
+++ src/main/java/com/proyecto/b/s/controller/AnswerController.java
@@ -0,0 +1,33 @@
+package com.proyecto.b.s.controller;
+
+import com.proyecto.b.s.entity.Answer;
+import com.proyecto.b.s.service.service.AnswerService;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/bs/answer")
+@CrossOrigin("*")
+public class AnswerController {
+
+    private final AnswerService answerService;
+
+    public AnswerController(AnswerService answerService) {
+        this.answerService = answerService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Answer>> findAnswerList(){
+        return ResponseEntity.status(HttpStatus.OK).body(answerService.listAnswer());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Answer> findAnswerById(@PathVariable Long id) throws Exception{
+        return ResponseEntity.status(HttpStatus.OK).body(answerService.findById(id));
+    }
+
+}
---
+++ src/main/java/com/proyecto/b/s/repository/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.proyecto.b.s.repository;
+
+import com.proyecto.b.s.entity.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+}
