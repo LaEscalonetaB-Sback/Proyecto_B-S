a6f8203cdb701563fe22a1f10fe6d96e57301252
MirnaPamelaDiaz <pamela_diaz_24@outlook.com>
2023-04-14 08:09:00 -0300

    Feature se agregan dtos de evento

---
+++ src/main/java/com/proyecto/b/s/dto/response/eventResponseDTO/IndustryForEventResponseDTO.java
@@ -0,0 +1,10 @@
+package com.proyecto.b.s.dto.response.eventResponseDTO;
+
+import lombok.Data;
+
+@Data
+public class IndustryForEventResponseDTO {
+
+    private Long id;
+    private String name;
+}
