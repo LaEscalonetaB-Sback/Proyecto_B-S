034a01f7c6b30e0e26cd22901026d5158b819097
jasabira <jasbircoro.dev@gmail.com>
2023-04-22 13:27:14 -0300

    Update, eliminando espacios

--- src/main/java/com/proyecto/b/s/repository/PersonRepository.java
+++ src/main/java/com/proyecto/b/s/repository/PersonRepository.java
@@ -1,6 +1,5 @@
 package com.proyecto.b.s.repository;
 
-
 import com.proyecto.b.s.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,10 +9,8 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
-
 
     @Query(value = "SELECT DISTINCT p FROM Person p " +
             "LEFT JOIN p.roles r " +
@@ -39,8 +36,5 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
             @Param("roles") List<String> roles,
             @Param("skills") List<String> skills);
 
-
     Optional<Person> findByDniOrCuilOrEmailOrLinkedin(String dni, String cuil, String email, String linkedin);
-
-
 }
