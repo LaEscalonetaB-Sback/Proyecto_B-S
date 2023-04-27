32bb490964cdfb181ce9d3d961247c44f0456256
jasabira <jasbircoro.dev@gmail.com>
2023-04-27 12:52:45 -0300

    Update test

---
+++ src/test/java/com/proyecto/b/s/service/serviceImpl/SearchServiceImplTest.java
@@ -0,0 +1,35 @@
+package com.proyecto.b.s.service.serviceImpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+class SearchServiceImplTest {
+
+
+    @Test
+    void testListSearch() {
+    }
+
+    @Test
+    void saveSearch() {
+    }
+
+    @Test
+    void existById() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteSearch() {
+    }
+}
