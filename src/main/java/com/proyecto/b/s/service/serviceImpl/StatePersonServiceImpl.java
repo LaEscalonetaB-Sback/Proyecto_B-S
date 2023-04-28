ed245ea0cff050d3a2e5b91cf532a2bf5397ae6d
jasabira <jasbircoro.dev@gmail.com>
2023-04-28 15:07:00 -0300

    update merge feature_busqueda

--- src/main/java/com/proyecto/b/s/service/serviceImpl/StatePersonServiceImpl.java
+++ src/main/java/com/proyecto/b/s/service/serviceImpl/StatePersonServiceImpl.java
@@ -1,8 +1,7 @@
 package com.proyecto.b.s.service.serviceImpl;
 
-import com.proyecto.b.s.service.service.StatePersonService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class StatePersonServiceImpl implements StatePersonService {
+public class StatePersonServiceImpl {
 }
